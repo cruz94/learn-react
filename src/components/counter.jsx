ad01d9727872faf3966e58d7e6d221a4d2f63799
pradeep <praveencruz28@gmail.com>
2021-01-12 22:33:10 +0530

    session : state variable

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,22 +4,42 @@ import React, {Component} from "react";
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        tag: ['tag1', 'tag2', 'tag3']
     };
 
-    renderTag() {
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+  /*  constructor() {
+        super();
+        console.log("const",this.state);
+        this.handleClick = this.handleClick.bind(this);
+    }*/
+
+    handleClick = () => {
+        this.setState({ count:this.state.count + 1});
+        console.log("Event handled!",this.count);
     }
 
     render() {
         return (
             <div>
-                {this.renderTag()}
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleClick} className="btn btn-secondary btn-sm ">Inc</button>
+                <ul>
+                    {this.state.tag.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </div>
         );
     }
 
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
 }
 
 export default Counter;
