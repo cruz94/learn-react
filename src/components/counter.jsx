ffe81c40eed7f6a07e776274d47875ae04599489
pradeep <praveencruz28@gmail.com>
2021-01-14 22:44:00 +0530

    session : - passing variable - props - children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         tag: ['tag1', 'tag2', 'tag3']
     };
 
@@ -14,30 +14,31 @@ class Counter extends Component {
     }*/
 
     handleClick = () => {
-        this.setState({ count:this.state.count + 1});
-        console.log("Event handled!",this.count);
+        this.setState({ value:this.state.value + 1});
+        console.log("Event handled!",this.value);
     }
 
     render() {
         return (
             <div>
+                {this.props.children}
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleClick} className="btn btn-secondary btn-sm ">Inc</button>
-                <ul>
+             {/*   <ul>
                     {this.state.tag.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                </ul>*/}
             </div>
         );
     }
 
     formatCount() {
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+        const {value} = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 }
